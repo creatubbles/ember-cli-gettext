c7b0943f3339a582eb056b2a682c21d090e1490f
Marcin Chochowski <marcin.chochowski@useo.pl>
2016-12-08 15:00:19 +0100

    escape values in hash unless they're SafeString

--- addon/helpers/i18n-n.js
+++ addon/helpers/i18n-n.js
@@ -2,6 +2,11 @@ import Ember from 'ember';
 import gettext from 'ember-cli-gettext/gettext';
 
 export function getText(params, hash) {
+  for (var key in hash) {
+    if (!(hash[key] instanceof Ember.Handlebars.SafeString)) {
+      hash[key] = Ember.Handlebars.Utils.escapeExpression(hash[key]);
+    }
+  }
   return new Ember.Handlebars.SafeString(gettext._n(...params, hash));
 }
 
